744527c6e48fdf069789933d0d104b88ed911a2c
Nikita <gweegr44@gmail.com>
2026-06-12 15:13:28 +0500

    Точка входа и сборка всех модулей

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import asyncio
+from telegram.ext import ApplicationBuilder
+from Config import TG_BOT_TOKEN, DB_PATH
+from Storage import Storage
+from Handlers import setup_handlers
+from Scheduler import start_scheduler
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+async def main():
+    storage = Storage(DB_PATH)
+    app = ApplicationBuilder().token(TG_BOT_TOKEN).build()
+    handlers = setup_handlers(storage)
+    app.add_handlers(handlers)
+    start_scheduler(app.bot, storage)
+    print("Бот запущен. Ожидаю команды...")
+    await app.run_polling()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
